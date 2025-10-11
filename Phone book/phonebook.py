02c2f438a519d1eefe6a198244ec2702e5f56928
ru.sitdikov <rsitdikov03@gmail.com>
2025-10-10 18:44:56 -0700

    Create functions

--- Phone book/phonebook.py
+++ Phone book/phonebook.py
@@ -20,3 +20,57 @@ def init_db():
         conn.commit()
 
 
+def add_contact(name: str, phone: str, email: Optional[str] = None):
+    with sqlite3.connect(DB_NAME) as conn:
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO contacts (name, phone, email) VALUES (?, ?, ?)", (name, phone, email))
+        conn.commit()
+        print("Контакт добавлен.")
+
+
+def get_all_contacts() -> List[Dict]:
+    with sqlite3.connect(DB_NAME) as conn:
+        conn.row_factory = sqlite3.Row  # позволяет обращаться по имени столбца
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM contacts")
+        return [dict(row) for row in cursor.fetchall()]
+
+
+def update_contact(contact_id: int, name: str, phone: str, email: Optional[str] = None):
+    with sqlite3.connect(DB_NAME) as conn:
+        cursor = conn.cursor()
+        cursor.execute(
+            "UPDATE contacts SET name = ?, phone = ?, email = ? WHERE id = ?",
+            (name, phone, email, contact_id)
+        )
+        if cursor.rowcount == 0:
+            print("Контакт с таким ID не найден.")
+        else:
+            conn.commit()
+            print("Контакт обновлён.")
+
+
+def delete_contact(contact_id: int):
+    with sqlite3.connect(DB_NAME) as conn:
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM contacts WHERE id = ?", (contact_id,))
+        if cursor.rowcount == 0:
+            print("Контакт с таким ID не найден.")
+        else:
+            conn.commit()
+            print("Контакт удалён.")
+
+
+
+def show_contacts():
+    contacts = get_all_contacts()
+    if not contacts:
+        print("Телефонная книга пуста.")
+        return
+
+    print("\n--- Все контакты ---")
+    for c in contacts:
+        email_str = f", email: {c['email']}" if c['email'] else ""
+        print(f"ID: {c['id']} | Имя: {c['name']} | Телефон: {c['phone']}{email_str}")
+
+
